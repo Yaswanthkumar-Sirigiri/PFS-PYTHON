ca5088a5df7f57f9b4ec08b6b6c2433ebd9ee6a6
Yaswanth kumar Sirigiri <yaswanthkumarsirigiri@gmail.com>
2026-07-06 22:49:07 +0530

    DONE!!!

---
+++ Day-8.py
@@ -0,0 +1,118 @@
+'''a = 2
+b = 4
+print(a+b)'''
+
+'''a = 6
+b = 8
+c = a+b
+print(c)'''
+
+#print(10+10)
+
+#RUN TIME INPUT()
+
+'''a = int(input("Enter value: "))
+b = int(input("Enter value: "))
+print(a+b)'''
+
+'''a = float(input("enter the values: "))
+b = float(input("enter the values: "))
+print(a+b)'''
+
+'''a = str(input("enter data1: "))
+b = str(input("enter the data2: "))
+print(a+" "+b)'''
+
+
+
+'''a = int(input("a value: "))
+b = int(input("b value: "))
+option = int(input("choose the option 1.add 2.sub 3.mul 4.div: "))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+
+'''a = int(input("a value: "))
+b = int(input("b value: "))
+option = int(input("choose the option
+                        1.add
+                        2.sub
+                        3.mul
+                        4.div: "))
+print(a+b)
+print(a-b)
+print(a*b)'''
+
+
+
+#~~~~~~~~~~~~~SWAPPING OF TWO NUMBERS~~~~~~~~
+#~~~~~~~~~~~~~4 METHODS~~~~~~~~~~~~~~~~~
+'''a = int(input("Enter the value:"))
+b = int(input("Enter the value:"))
+temp = a
+a = b
+b = temp
+print(a,b)'''
+
+
+'''a = int(input("Enter the value:"))
+b = int(input("Enter the value:"))
+a,b = b,a
+print(a,b)'''
+
+
+
+'''a = int(input("Enter the values:"))
+b = int(input("Enter the values:"))
+a = a+b
+b = a-b
+a = a-b
+print(a,b)'''
+
+
+
+'''a = int(input("Enter the values:"))
+b = int(input("Enter the values:"))
+a = a+b
+b = a-b
+a = a-b
+print("after swapping a=%d, b=%d"%(a,b))'''
+
+
+
+'''a = float(input("Enter the values:"))
+b = float(input("Enter the values:"))
+a = a+b
+b = a-b
+a = a-b
+print("after swapping a=%f, b=%f"%(a,b))'''
+
+
+
+a = str(input("Enter the values:"))
+b = str(input("Enter the values:"))
+a,b = b,a
+print("after swapping a=%s, b=%s"%(a,b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+    
+ 
