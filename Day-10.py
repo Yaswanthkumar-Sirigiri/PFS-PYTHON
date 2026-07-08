ec4f6f08b1695b156f33fb745b194b32ab6751df
Yaswanth kumar Sirigiri <yaswanthkumarsirigiri@gmail.com>
2026-07-08 22:19:25 +0530

    DONE!!!

---
+++ Day-10.py
@@ -0,0 +1,276 @@
+#IF-ELSE CONDITION USING COMPARISION OPERATOR
+'''a = 3
+b = 6
+if a<b:
+    print("True")
+else:
+    print("False")'''
+
+
+'''a = 3
+b = 6
+if a>b:
+    print("True")
+else:
+    print("False")'''
+
+
+'''a = 9
+b = 12
+if a!=b:
+    print("Not equal")
+else:
+    print("True")'''
+
+
+#IF-ELSE CONDITION USING LOGICAL OPERATOR
+'''a = 5
+b = 10
+if a<b and b>a:
+    print("Less")
+else:
+    print("True")'''
+
+
+'''a = 5
+b = 6
+if a<b or a==b:
+    print(True)
+else:
+    print(False)'''
+
+
+'''a = 5
+b = 7
+if not a>b and a<b:
+    print(True)
+else:
+    print(False)'''
+
+
+#IF-ELSE CONDITION USING IDENTIFY OPERATOR
+'''a = 5
+if type(a) is int:
+    print(True)
+else:
+    print(False)'''
+
+
+a = "5"
+'''if type(a) is int:
+    print(True)
+else:
+    print(False)'''
+
+
+'''a = 12.5
+if type(a) is  not str:
+    print(True)
+else:
+    print(False)'''
+
+
+'''a = "Hello"
+if type(a) is not str:
+    print(True)
+else:
+    print(False)'''
+
+
+#IF-ELSE CONDITION USING MEMBERSHIP OPERATOR
+'''a = 2,3,4,5,6,7,8,9,10
+if 3 in a:
+    print(True)
+else:
+    print(False)'''
+
+
+'''a = 2,3,4,5,6,8,7,9
+if 22 not in  a:
+    print(True)
+else:
+    print(False)'''
+
+
+#IF-ELIF-ELSE CONDITION USING COMPARISION OPERATOR
+'''a = 8
+b = 10
+if a<b:
+    print("Less")
+elif b>a:
+    print("Greater")
+else:
+    print("True")'''
+
+
+'''a = 8
+b = 10
+if a==b:
+    print("Equal")
+elif a<b:
+    print("Less")
+elif a>b:
+    print("Greater")
+elif a!=b:
+    print("Not equal")
+else:
+    print("False")'''
+
+
+#IF-ELIF-ELSE CONDITION USING LOGICAL OPERATOR
+'''a = 5
+b = 10
+if a<b and b>a:
+    print("a Less")
+elif b<a or a<b:
+    print("True")
+elif not a<b or a<b:
+    print("Not")
+else:
+    print("False")'''
+
+
+#IF-ELIF-ELSE CONDITION USING IDENTIFY OPERATOR
+'''a = 12
+if type(a) is int:
+    print("INT")
+elif type(a) is not float:
+    print("Not Float")
+else:
+    print(False)'''
+
+
+#IF-ELIF-ELSE CONDITION USING MEMBERSHIP OPERATOR
+'''a = 1,2,3,4,5,6,7,8,9,10
+if 3 in a:
+    print(True)
+elif 5 not in a:
+    print("Not")
+else:
+    print(False)'''
+
+
+#MULTIPLE-IF CONDITION USING ARITHMATIC OPERATOR
+'''a = 10
+b = 20
+if a<b:
+    print("a Less")
+if b>a:
+    print("b Greater")
+else:
+    print(False)'''
+
+
+#MULTIPLE-IF CONDITION USING LOGICAL OPERATOR
+'''a = 10
+b = 20
+if a<b and b>a:
+    print("a Less")
+if a!=b or a==b:
+    print("OR")
+else:
+    print(True)'''
+
+
+#MULTIPLE-IF CONDITION USING IDENTIFY OPERATOR
+'''a = "23"
+if type(a) is str:
+    print("a is sring")
+if type(a) is not int:
+    print("a is not integer")
+else:
+    print(False)'''
+
+
+#MULTIPLE-IF CONDITION USING MEMBERSHIP OPERATOR
+'''a = 1,2,3,4,5,6,7,8,9,10
+if 3 in a:
+    print("3 is in a")
+if 5 in a:
+    print("5 is in a")
+if 22 not in a:
+    print("22 not in a")
+else:
+    print(False)'''
+
+
+#NESTED-IF
+'''a = 4
+b = 6
+if a<b:
+    print("Less")
+    if b>a:
+        print("Greater")'''
+
+
+'''a = 4
+b = 6
+if a>b:
+    print("Less")
+    if b>a:
+        print("Greater")'''
+
+
+'''a = 7
+b = 12
+if a<b:
+    print("Less")
+    if b==a:
+        print("Greater")'''
+
+
+'''a = 12
+b = 13
+if a<b:
+    print("Less")
+    if b==a:
+        print("Greater")
+    else:
+        print("True")'''
+
+
+'''a = 15
+b = 17
+if a>b:
+    print("Less")
+    if b>a:
+        print("Greater")
+else:
+    print("True")'''
+
+
+'''a = 9
+b = 11
+if a<b:
+    print("Less")
+    if b>a:
+        print("Greater")
+    else:
+        print("True")
+else:
+    print("false")'''
+
+
+'''a = 4
+b = 6
+if a<b:
+    print("Less")
+    if b==a:
+        print("Greater")
+    elif a!=b:
+        print("Not equal")'''
+
+
+'''a = 4
+b = 6
+if a<b:
+    print("Less")
+    if b==a:
+        print("Greater")
+    elif a>=b:
+        print("Not equal")
+    else:
+        print("equal")'''
+
+        
+
