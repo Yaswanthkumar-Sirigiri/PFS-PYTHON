34be6bf800c86d7e671fa57a1d18c0357451036f
Yaswanth kumar Sirigiri <yaswanthkumarsirigiri@gmail.com>
2026-07-09 22:45:59 +0530

    DONE!!!

---
+++ Day-11.py
@@ -0,0 +1,115 @@
+#~~~~~~~~~~~~~~TASK-1~~~~~~~~~~~~~~~
+'''age = int(input("Enter age: "))
+if age>18:
+    print("Congrats!!, You are eligible for vote.")
+else:
+    print("Oops!, You are not eligible to vote.")'''
+
+
+#~~~~~~~~~~~~~TASK-2~~~~~~~~~~~~~~~
+'''num = int(input("Enter the number: "))
+if num%2==0:
+    print(f"{num} is Even number.")
+else:
+    print(f"{num} is Odd number.")'''
+
+
+#~~~~~~~~~~~~TASK-3~~~~~~~~~~~~~~~~~~
+'''year = int(input("Enter the year to be checked: "))
+if year%4==0:
+    print("leap year")
+else:
+    print("Not leap year")'''
+
+
+#~~~~~~~~~~~TASK-4~~~~~~~~~~~~~
+'''name = input("Enter your name: ").lower()
+if name=="yaswanth":
+    print("Welcome",name)
+else:
+    print("welcome guest")'''
+
+
+#~~~~~~~~~~TASK-5~~~~~~~~~~~~~~
+'''name = input("Enter your name: ").lower()
+known = ["yaswanth","sahim","raghu","ajith","mukesh"]
+if name in known:
+    print("Welcome",name)
+else:
+    print("welcome guest")'''
+
+
+#~~~~~~~~~TASK-6~~~~~~~~~~~
+'''name = input("Enter your name: ").lower()
+known = ["a","e","i","o","u"]
+if name in known:
+    print("Ovel",name)
+else:
+    print("consonent")'''
+
+
+
+#~~~~~~~~~~TASK-7~~~~~~~~~~
+'''username = input("Enter Username: ").lower()
+password = input("Enter Password: ").lower()
+if username=="yaswanth":
+    if password=="1234":
+        print("login successful")
+    else:
+        print("Invalid")
+else:
+    print("invalid credentials")'''
+
+
+'''username = input("enter the username")
+password = input("enter the password")
+if username=="yaswanth" and password=="yaswanth@1234":
+    print("login successful")
+else:
+    print("invalid")'''
+
+
+#~~~~~~~~~~~TASK-8~~~~~~~~~~~~~~~
+'''age = int(input("Enter your age: "))
+marks = float(input("Enter your marks: "))
+attendance = int(input("Enter your attendance: "))
+if age>18:
+    print("Eligible to vote")
+if marks>80:
+    print("Eligible for scholarship")
+if attendance>70:
+    print("Eligible")
+else:
+    print("False")'''
+
+
+#~~~~~~~~~~~~~TASK-9~~~~~~~~~~~~~~
+'''price = int(input("Enter price of Cake: "))
+if price == 1000:
+    print("Red velvet cake.")
+elif price == 900:
+    print("Chocolate Cake.")
+elif price == 600:
+    print("ButterScoch Cake.")
+else:
+    print("Cake not available.")'''
+
+
+#~~~~~~~~~~~~TASK-10~~~~~~~~~~~~~~~~
+pizza = input("Enter pizza: ").lower()
+if pizza == "bbq pizza":
+    print("RS.1000/-")
+elif pizza == "crispy pizza":
+    print("RS.900/-")
+elif pizza == "panneer pizza":
+    print("RS.600/-")
+elif pizza == "corn pizza":
+    print("RS.400/-")
+elif pizza == "french fries pizza":
+    print("RS.200/-")
+else:
+    print("Pizza Not Available.")
+
+          
+        
+    
