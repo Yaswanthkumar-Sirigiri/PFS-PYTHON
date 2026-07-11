45a24df34e88a6bddc3dd7c068654efe58d89b27
Yaswanth kumar Sirigiri <yaswanthkumarsirigiri@gmail.com>
2026-07-11 22:55:31 +0530

    DONE!!!

---
+++ Day-13.py
@@ -0,0 +1,210 @@
+#WHILE LOOP()
+'''a = 10
+while a>1:
+    print(a)''' #infinite loop
+
+
+'''a = 10
+while a>1:
+    print(a)
+    a = a-1'''
+
+
+'''a = 10
+while a>=1:
+    print(a)
+    a = a-1'''
+
+
+'''a = 10
+while a>1:
+    a = a-1
+    print(a)'''
+
+
+'''a = 10
+while a>1:
+    a = a-1
+    print(a)'''
+
+
+'''a = 20
+while a>5:
+    a = a-1
+print(a)'''
+
+
+'''a = 30
+while a>2:
+    print(a)  #assignment operator
+    a+=1'''   #infinite loop
+
+
+'''a = 30
+while a>2:
+    print(a)
+    a-=1'''
+
+
+'''a = 5
+while a<25:
+    print(a)
+    a+=1'''
+
+
+#VOTING TASK
+'''while True:
+    age = int(input("Enter your age: "))
+    if age>=18:
+        print("Eligible for vote.")
+    else:
+        print("Not Eligible.")'''
+
+
+#EVEN OR ODD TASK        
+'''while True:
+    num = int(input("Enter the number: "))
+    if num%2==0:
+        print("It is even.")
+    else:
+        print("It is odd.")'''
+
+
+#RANGE()
+#START-STOP-STEP
+'''for i in range(10):
+    print(i)'''
+
+
+'''for i in range(5,15):
+    print(i)'''
+
+
+'''for i in range(30,45):
+    print(i,end=",")'''
+
+
+#TASKS:
+'''for i in range(2,20,2):
+    print(i,end=",")'''
+
+
+'''for i in range(5,50,5):
+    print(i,end=",")'''
+
+
+'''for i in range(0,30,3):
+    print(i,end=",")'''
+
+
+#GRADE TASK
+'''
+while True:
+marks = int(input("Enter Marks: "))
+if marks in range(91,101):
+    print("Grade-A")
+elif marks in range(81,91):
+    print("Grade-B")
+elif marks in range(71,81):
+    print("Grade-C")
+elif marks in range(50-71):
+    print("Grade-D")
+else:
+    print("Fail.")'''
+
+
+#BREAK
+'''a = 20
+while a>5:
+    print(a)
+    a = a-1
+    if a==10:
+        break'''
+
+
+'''a = 30
+while a>2:
+    a = a-1
+    if a==20:
+        break
+    print(a)'''
+
+
+'''for i in range(40,65):
+    if i==55:
+        break
+    print(i)'''
+
+
+'''a = "python"
+if a=="h":
+    break
+print(a)''' #error
+
+
+'''a = "python"
+for i in a:
+    if i=="h":
+        break
+    print(i)'''
+
+
+#CONTINUE
+'''a = 15
+while a>3:
+    print(a)
+    a=a-1
+    if a==11:
+        continue''' #11 prints without skip
+
+
+'''a = 15
+while a>3:
+    a = a-1
+    if a==11:
+        continue
+    print(a)'''
+
+
+'''for i in range(18):
+    if i==14:
+        continue
+    print(i)'''
+
+
+'''a = "python"
+for i in a:
+    if i=="t":
+        continue
+    print(i)'''
+
+
+#PASS
+'''a = 12
+while a>4:
+    print(a)
+    a=a-1
+    if a==10:
+        pass'''
+
+
+'''for i in range(25):
+    if i==10:
+        print(i)'''
+
+
+'''for i in range(25):
+    if i==10:
+        pass''' #holds the data without any errors.
+
+
+
+
+
+
+
+
+
+        
+
+
