2f407687cc57b4b4648366d4cec0430775b59f50
Yaswanth kumar Sirigiri <yaswanthkumarsirigiri@gmail.com>
2026-07-25 19:51:29 +0530

    DONE!!!

---
+++ Day-24/data.py
@@ -0,0 +1,14 @@
+'''import mymodule
+mymodule.greetings("Yaswanth")'''
+
+#import mymodule
+
+#import mymodule
+
+import mymodule
+'''a = mymodule.details["names"]
+b = mymodule.details["idnos"]
+print(a,b)
+print(b)'''
+
+
---
+++ Day-24/mymodule.py
@@ -0,0 +1,43 @@
+'''def greetings(name):
+    print("welcome",name)'''
+
+'''a = 3
+b = 8
+print("The sum is",a+b)'''
+
+'''a = int(input("a value: "))
+b = int(input("b value: "))
+print(a+b)'''
+
+'''details = {"idnos":[10,20,30],
+           "names":["Yaswanth","Kumar","Sirigiri"],
+           "marks":[60,70,90]}'''
+
+'''if __name__=="__main__":
+    a = [10,20,30,40,50]
+    a.append("code")
+    a.extend("code")
+    print(a)'''
+
+'''def dummy():
+    if __name__=="__main__":
+        print("This program is run as script.")
+    else:
+        print("This program is run as Module.")
+dummy()'''
+
+
+#Task:
+'''def Hello():
+    a = int(input("a value: "))
+    b = int(input("b value: "))
+    print("sum: ",a+b)
+    inp = int(input("0 or 1: "))
+    if inp==0:
+        Hello()
+    if inp==1:
+        return
+Hello()'''
+    
+
+
